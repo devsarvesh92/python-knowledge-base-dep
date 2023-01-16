4b5194e1b7fa52254c150f62a3e81cc7ebb3105b
sarvesh.sawant <sarveshdev92@gmail.com>
2023-01-16 20:39:34 +0530

    Add queue

---
+++ dsal/queue.py
@@ -0,0 +1,34 @@
+from collections import deque
+
+class Queue:
+    """
+    Creates Queue
+    """
+
+    def __init__(self) -> None:
+        self.col = deque()
+
+    def enque(self,val):
+        self.col.appendleft(val)
+    
+    def pop(self):
+        self.col.pop()
+
+    def __len__(self):
+        return self.col.__len__()
+
+    def __str__(self) -> str:
+        elements = ""
+        for el in self.col:
+           elements += f"{el} --> "
+        
+        return elements
+
+q = Queue()
+q.enque(10)
+q.enque(11)
+q.enque(12)
+print(q)
+q.pop()
+
+print(q)
